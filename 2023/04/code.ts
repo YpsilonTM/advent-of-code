dfb0b87a61404de5df19700f434af507119d918d
YpsilonTM <yurihackl@gmail.com>
2023-12-06 13:45:14 +0100

    Day 4 Part 2

--- 2023/04/code.ts
+++ 2023/04/code.ts
@@ -5,30 +5,63 @@ const input = readFileSync('./2023/04/input.txt', 'utf-8')
 const tickets = input
   .trim()
   .split('\n')
-  .map((line) => line.trim())
-
-const scratchtickets = tickets.map((ticket) => {
-  const [cardname, numbers] = ticket.trim().split(':')
-  const numbersplit = numbers.trim().split('|')
-  const playnumbers = numbersplit[0]
-    .trim()
-    .split(' ')
-    .filter((val) => val)
-  const winningnumbers = numbersplit[1]
-    .trim()
-    .split(' ')
-    .filter((val) => val)
-
-  let winningNumbers = 0
-  playnumbers.forEach((number) => {
-    if (winningnumbers.includes(number)) {
-      winningNumbers++
-    }
+  .map((line) =>
+    line
+      .trim()
+      .substring(line.indexOf(':') + 1)
+      .split('|')
+      .map((numbers) =>
+        numbers
+          .trim()
+          .split(' ')
+          .filter((val) => val)
+      )
+  )
+  .map(([playnumbers, winningnumbers]) => {
+    return { count: 1, wins: winningnumbers.filter((number) => playnumbers.includes(number)).length }
   })
+  .map(({ count, wins }, index, cards) => {
+    for (let card = index + wins; card > index; card--) {
+      cards[card].count += count
+    }
+    return count
+  })
+  .reduce((accumulator, value) => accumulator + value)
 
-  const score = winningNumbers == 0 ? 0 : Math.pow(2, winningNumbers - 1)
-  return { cardname, playnumbers, winningnumbers, winningNumbers, score }
-})
+console.log(tickets)
 
-const totalscore = scratchtickets.reduce((acc, ticket) => acc + ticket.score, 0)
-console.log(totalscore)
+// const extraCards: number[] = []
+
+// const scratchtickets = tickets.map((ticket) => {
+//   const [cardname, numbers] = ticket.trim().split(':')
+//   const ticketnumber = Number(cardname.trim().split(' ')[1])
+//   const numbersplit = numbers.trim().split('|')
+//   const playnumbers = numbersplit[0]
+//     .trim()
+//     .split(' ')
+//     .filter((val) => val)
+//   const winningnumbers = numbersplit[1]
+//     .trim()
+//     .split(' ')
+//     .filter((val) => val)
+
+//   let winningNumbers = 0
+//   playnumbers.forEach((number) => {
+//     if (winningnumbers.includes(number)) {
+//       winningNumbers++
+//     }
+//   })
+
+//   for (let i = 1; i <= winningNumbers; i++) {
+//     extraCards.push(ticketnumber + i)
+//   }
+
+//   const score = winningNumbers == 0 ? 0 : Math.pow(2, winningNumbers - 1)
+//   return { ticketnumber, cardname, playnumbers, winningnumbers, winningNumbers, score }
+// })
+
+// // console.log(scratchtickets)
+// console.log(extraCards)
+
+// const totalscore = scratchtickets.reduce((acc, ticket) => acc + ticket.score, 0)
+// // console.log(totalscore)
