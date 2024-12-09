e1632b66c254f3f3b49085d06fcd69efe14261e6
Ypsilon <22861150+YpsilonTM@users.noreply.github.com>
2024-12-09 11:24:45 +0100

    chore: 2023 Day 5 Part 1

--- 2023/05/code.ts
+++ 2023/05/code.ts
@@ -1,18 +1,41 @@
 import { readFileSync } from 'fs'
+import { join } from 'path'
 
-const input = readFileSync('./example.txt', 'utf-8')
+const input = readFileSync(join(__dirname, 'input.txt'), { encoding: 'utf-8', flag: 'r' })
 
-// get everything on the line with'seeds:
-const seeds = input
-  .match(/seeds: (.*)/)?.[1]
-  .split(' ')
-  .map(Number)
+function mapResult(input: number, map: number[][]) {
+  let soil = input
+  map.forEach(([soilStart, seedStart, rangeLenght]) => {
+    const seedEnd = seedStart + rangeLenght
+    if (input >= seedStart && input < seedEnd) {
+      soil = soilStart + (input - seedStart)
+    }
+  })
+  return soil
+}
 
-// Get all the lines after seed-to-soil map: until the first empty line
-const soilMap = input
-  .trim()
-  .replace('\r', '')
-  .match(/seed-to-soil map:\n(.*?)\n\n/gm)?.[1]
-  .split('\n')
+const instructions = input.split('\r\n\r\n').map((line) =>
+  line
+    .split(':')[1]
+    .trim()
+    .split('\r\n')
+    .map((row) => row.split(' ').map((val) => parseInt(val)))
+)
 
-console.log(soilMap)
+const seeds = instructions[0][0]
+
+const results = seeds.map((seed) => {
+  const soil = mapResult(seed, instructions[1])
+  const fertilizer = mapResult(soil, instructions[2])
+  const water = mapResult(fertilizer, instructions[3])
+  const light = mapResult(water, instructions[4])
+  const temperature = mapResult(light, instructions[5])
+  const humidity = mapResult(temperature, instructions[6])
+  const location = mapResult(humidity, instructions[7])
+
+  return { seed, soil, fertilizer, water, light, temperature, humidity, location }
+})
+
+const lowestLocation = results.reduce((acc, curr) => (curr.location < acc.location ? curr : acc))
+
+console.log(lowestLocation.location)
