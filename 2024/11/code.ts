49fabb3f3ef8e32687080030f582c23657a6a088
YpsilonTM <22861150+YpsilonTM@users.noreply.github.com>
2024-12-11 15:52:31 +0100

    chore: 2024 Day 11 Part 1

--- 2024/11/code.ts
+++ 2024/11/code.ts
@@ -2,9 +2,31 @@ import { readFileSync } from 'fs'
 import { join } from 'path'
 
 const data = readFileSync(join(__dirname, 'input.txt'), { encoding: 'utf-8', flag: 'r' })
-const lines = data.split('\n')
+let numbers = data.split(' ').map((number) => parseInt(number) || 0)
 
-function part1() {}
+function blink(numbers: number[]) {
+  const result: number[] = []
+  for (const number of numbers) {
+    if (number === 0) {
+      result.push(1)
+    } else if (number.toString().length % 2 === 0) {
+      const strNum = number.toString()
+      const half = strNum.length / 2
+      result.push(parseInt(strNum.slice(0, half)) || 0, parseInt(strNum.slice(half)) || 0)
+    } else {
+      result.push(number * 2024)
+    }
+  }
+  return result
+}
+
+function part1() {
+  for (let i = 0; i < 25; i++) {
+    numbers = blink(numbers)
+    console.log('Blink #' + i, numbers.length)
+  }
+  return numbers.length
+}
 
 function part2() {}
 
