99a68251183321357d89c99b80ae6b519a047f02
Ypsilon <22861150+YpsilonTM@users.noreply.github.com>
2025-02-20 11:49:37 +0100

    chore: 2024 Day 12 Part 1 (not finished)

--- 2024/12/code.ts
+++ 2024/12/code.ts
@@ -1,10 +1,31 @@
 import { readFileSync } from 'fs'
 import { join } from 'path'
 
-const data = readFileSync(join(__dirname, 'input.txt'), { encoding: 'utf-8', flag: 'r' })
-const lines = data.split('\n')
+const data = readFileSync(join(__dirname, 'example.txt'), { encoding: 'utf-8', flag: 'r' })
+const gardenMap = data.split('\n').map((line) => line.trim().split(''))
 
-function part1() {}
+type Map = string[][]
+
+function loopMap(map: Map, cb: (x: number, y: number, value: string) => void) {
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      cb(x, y, map[y][x])
+    }
+  }
+}
+
+function part1() {
+  // Go over map and group adjecaent plants together
+  const areas = new Map<string, number[]>()
+
+  loopMap(gardenMap, (x, y, value) => {
+    if (areas.get(value) === undefined) {
+      areas.set(value, [])
+    } else {
+      areas.get(value)?.push(x, y)
+    }
+  })
+}
 
 function part2() {}
 
